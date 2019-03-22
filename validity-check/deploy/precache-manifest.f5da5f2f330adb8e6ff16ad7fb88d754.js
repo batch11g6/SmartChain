3f9699c314d5bfdc93484b2a833133d988db6a0f
dm212 <yashwanth212@gmail.com>
2019-03-22 11:04:40 +0530

    22-03-2019 ui update

---
+++ validity-check/deploy/precache-manifest.f5da5f2f330adb8e6ff16ad7fb88d754.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d5d888ce490592dd81b7a33c6b106f9",
+    "url": "/static/media/smartchain.4d5d888c.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "57e70e4eb0deeee8e637",
+    "url": "/static/js/main.57e70e4e.chunk.js"
+  },
+  {
+    "revision": "719cffe9009f9486bd01",
+    "url": "/static/js/1.719cffe9.chunk.js"
+  },
+  {
+    "revision": "57e70e4eb0deeee8e637",
+    "url": "/static/css/main.8272a32d.chunk.css"
+  },
+  {
+    "revision": "719cffe9009f9486bd01",
+    "url": "/static/css/1.75b5fa60.chunk.css"
+  },
+  {
+    "revision": "48897720802607180e237a078ca5b4dd",
+    "url": "/index.html"
+  }
+];
--- validity-check/src/pages/About.jsx
+++ validity-check/src/pages/About.jsx
@@ -19,26 +19,64 @@ export default class About extends Component {
       <div>
         <HeaderTemplate/>
         <SpaceBlock/>
-        <Card style={{width:"50%", margin:"auto" ,color:"gray"}} shadow={2}>
-          {/** Padding  TBRL */}
-        <div style={{padding:"10px 20px 10px 30px"}}>
-          <h1 >
-            Team 11, Group 6
-          </h1>
-          <h3>Team Members</h3>
-          <ul>
-            <li>Yashwanth D M </li>
-            <li>Phaneendra A R</li>
-            <li>Mayura K R</li>
-            <li>Nikil M</li>
-          </ul>
-          <Button  raised ripple colored onClick={this.handleGithubClick}>
-              GitHub
-          </Button>
-          
+
+        <div class="columns">
+            <div class="column">
+              {/** Empty column for space */}
+            </div>
+            <div class="column">
+
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">
+                    Smart Chain 
+                </p>
+                <a href="#" class="card-header-icon" aria-label="more options">
+                <span class="icon">
+                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+                </a>
+              </header>
+              <div class="card-content">
+              <div class="content">
+                {/*Scanner block*/}  
+                <br/>
+                <h1>Project Mentor: Sangeetha V</h1>
+
+                <h1>Tream Members</h1>
+                <table>
+                  <thead>
+                    <th>Name</th>
+                    <th>USN</th>
+                  </thead>
+                  <tr>
+                    <td>Yashwanth D M</td>
+                    <td>1KS15CS123</td>
+                  </tr>
+                  <tr>
+                    <td>Nikil M</td>
+                    <td>1KS15CS066</td>
+                  </tr>
+                  <tr>
+                    <td>Mayura K R</td>
+                    <td>1KS15CS056</td>
+                  </tr>
+                  <tr>
+                    <td>Phaneendra A R</td>
+                    <td>1KS15CS002</td>
+                  </tr>
+                </table>
+              </div>
+            <a class="button is-link is-rounded" onClick={this.handleGithubClick}>GitHub</a>
+            </div>  
+          </div>
+
+            </div>
+            <div class="column">
+                {/** Empty column for space */}
+            </div>
         </div>
-        </Card>
-      </div>
+        </div>
     )
   }
 }
